3cb5f03cbb6a8629925302742a219e6fa8e09db5
Romildo Jozue Paiter <romildo.paiter@sigma.com.br>
2016-10-28 16:51:02 -0300

    Exemplos Iniciais do Capitulo 3

---
+++ cap3/Test4.java
@@ -0,0 +1,9 @@
+class Test3 {
+
+	static int x = 15;
+
+	public static void main(String[] x) {
+		x = 200;
+		System.out.println(x);
+	}
+}
---
+++ cap3/Test5.java
@@ -0,0 +1,9 @@
+class Test5 {
+	static int i = 3;
+
+	public static void main(String[] a) {
+		for (new Test5().i = 10; new Test5().i < 100; new Test().i++) {
+			System.out.println(i);			
+		}
+	}
+}
